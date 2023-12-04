25e03a15a22ace45fc881e115803b1c7db296673
laurabacon <laura_kessinger@yahoo.com>
2023-12-03 17:36:31 -0700

    type value name input

--- script.js
+++ script.js
@@ -7,61 +7,57 @@ var pEl = document.querySelector('p');
 var inputDiv = document.getElementById('input-radio');
 
 var myQuestions = [
-  {
-    question: "What does CSS stand for?",
+{question: "What does CSS stand for?",
     choices: ["Cascading Style Sheets", "Cascading Soft Sheets", "Case Style Show", "Claustrophobia Style Sheet"],
     answer: 0
-  },
-  {
-    question: "Who invented JavaScript?",
+},
+{question: "Who invented JavaScript?",
     choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich", "Bill Gates"],
     answer: 2
-  },
-  {
-    question: "What is NOT a data type?",
+},
+{question: "What is NOT a data type?",
     choices: ["String", "Alphabetic", "Numeric", "Boolean"],
     answer: 1
-  },
-  {
+},
+{
     question: "Arrays in Javascript can be used to ___?",
-    choices: ["Objects", "Help Organize and Store Data", "Add Two Values Together", "None of the above"],
+    choices: ["Objects", "Help Organize and Store Data", "Write Code For You", "None of the above"],
     answer: 1
-  },
+},
 ];
 
 var currentQuestionIndex = -1;
 var currentAnswerIndex = -1;
 
 function beginQuiz() {
-  pEl.remove();
-
-  inputDiv.innerHTML = '';
-
-  var inputRadio1 = document.createElement("input");
-  inputRadio1.type = "radio";
-  inputRadio1.textContent = "Choice 1";
-  inputDiv.appendChild(inputRadio1);
-
-  startButton.textContent = "Next";
-  startButton.addEventListener("click", nextQuestion);
-  startButton.removeEventListener("click", beginQuiz);
-
-  for (i = 0; i < myQuestions.length; i++) {
-    var question = myQuestions[i].question;
+    pEl.remove();
+    startButton.textContent = "Next";
+    startButton.addEventListener("click", nextQuestion);
+    startButton.removeEventListener("click", beginQuiz);
+    var question = myQuestions[0].question;
     h1El.textContent = question;
-  }
-
-  nextQuestion();
+    var choices = myQuestions[0].choices;
+    for (var i = 0; i < choices.length; i++) {
+        var inputRadio = document.createElement("input");
+        inputRadio.type = "radio";
+        inputRadio.name = "answer";
+        inputRadio.value = i;
+        inputDiv.appendChild(inputRadio);
+        var label = document.createElement("label");
+        label.textContent = choices[i];
+        inputDiv.appendChild(label);
+    }
 }
 
+
 function nextQuestion() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < myQuestions.length) {
-    var question = myQuestions[currentQuestionIndex].question;
+    currentQuestionIndex++;
+    if (currentQuestionIndex < myQuestions.length) {
+        var question = myQuestions[currentQuestionIndex].question;
     h1El.textContent = question;
-  } else {
-    return;
-  }
+    } else {
+        return;
+    }
 }
 
 startButton.addEventListener("click", beginQuiz);
